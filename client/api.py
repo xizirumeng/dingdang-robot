9958ee54aac20c248d79bcd6d7b476601a77733a
xizirumeng <xizirumeng@qq.com>
2017-11-24 17:45:07 +0800

    debug

--- client/api.py
+++ client/api.py
@@ -5,18 +5,21 @@ import thread
 from bottle import route, run, template, request, error
 def api_service(mic, brain, user, password, port):
     logger = logging.getLogger(__name__)
-    @route('/dingdang/<name>', method = 'GET')
-    def index(name):
+    @route('/dingdang/', method = 'GET')
+    def index():
         u = request.query.u
         p = request.query.p
         if user != u or hashlib.md5(p).hexdigest() != hashlib.md5(password).hexdigest():
             return template('Incorrect username or password !', name=name)
         command = request.query.command
-        print command
         if '[control]' in command:
-            thread.start_new_thread(control, command)
+            # 执行命令
+            brain.query([command.replace('[control]','').strip()])
+            return '拍照成功！'
         elif '[echo]' in command:
-            thread.start_new_thread(echo, command)
+            # 说话
+            mic.say([command.replace('[echo]','').strip()])
+            return 'true'
         elif '[shell]' in command:
             mic.say(command.replace('[echo]', '').strip())
 
@@ -29,13 +32,6 @@ def api_service(mic, brain, user, password, port):
     @error(404)
     def mistake404(code):
         return 'Sorry, this page does not exist!'
-
-    def control(command):
-        # 执行叮当命令
-        brain.query([command.replace('[control]', '').strip()])
-    def echo(command):
-        # 执行叮当命令
-        mic.say([command.replace('[echo]', '').strip()])
     try:
         run(host='0.0.0.0', port=port)
     except Exception, e:
