dd1a2ee4d68d648518ec291c1301b6ad2cadeeea
RuMengRen <xizirumeng@gmail.com>
2017-11-24 18:06:31 +0800

    debug

--- client/api.py
+++ client/api.py
@@ -1,29 +1,32 @@
 # -*- coding: utf-8-*-
 import logging
 import hashlib
-import thread
-from bottle import route, run, template, request, error
+import subprocess
+from bottle import route, run, request, error
 def api_service(mic, brain, user, password, port):
     logger = logging.getLogger(__name__)
     @route('/dingdang/', method = 'GET')
     def index():
         u = request.query.u
         p = request.query.p
+        html = request.query.html
         if user != u or hashlib.md5(p).hexdigest() != hashlib.md5(password).hexdigest():
-            return template('Incorrect username or password !', name=name)
+            return '{"msg":"Incorrect username or password","success":false}'
         command = request.query.command
         if '[control]' in command:
             # 执行命令
             brain.query([command.replace('[control]','').strip()])
-            return '拍照成功！'
+            return '{"msg":"success","success":true}'
         elif '[echo]' in command:
             # 说话
             mic.say([command.replace('[echo]','').strip()])
-            return 'true'
+            return '{"msg":"success","success":true}'
         elif '[shell]' in command:
-            mic.say(command.replace('[echo]', '').strip())
-
-        return template('<b>Hello {{name}}</b>!', name=name)
+            p = subprocess.Popen(command.replace('[shell]','').strip(), shell = True, stdout = subprocess.PIPE)
+            out, err = p.communicate()
+            for line in out.splitlines():
+                return '{"msg":"success","success":true,"data":"'+ (html %line) +'"}'
+        return '{"msg":"success","success":true}'
 
     @error(403)
     def mistake403(code):
